55c8a71e78d681e749bf66be8c256f70abe8ed36
Anders W. Olsen <anders@wiberg.tech>
2017-01-31 18:10:59 +0100

    Adding classes

---
+++ src/Client.java
@@ -0,0 +1,8 @@
+/**
+ * Created by AndersWOlsen on 31-01-2017.
+ */
+public class Client {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/Server.java
@@ -0,0 +1,8 @@
+/**
+ * Created by AndersWOlsen on 31-01-2017.
+ */
+public class Server {
+    public static void main(String[] args) {
+
+    }
+}
