1510a4c8f33d98bd6b5e22e9566a47e15e3d1461
Anders W. Olsen <anders@wiberg.tech>
2017-01-31 18:20:20 +0100

    Basic Server

--- src/Client.java
+++ src/Client.java
@@ -1,8 +1,27 @@
 /**
  * Created by AndersWOlsen on 31-01-2017.
  */
-public class Client {
-    public static void main(String[] args) {
 
+import java.io.*;
+import java.net.*;
+
+public class Client {
+    public static void main(String[] args) throws Exception{
+        String msg;
+        String msgModified;
+
+        BufferedReader inUser = new BufferedReader(new InputStreamReader(System.in));
+        Socket sck = new Socket(args[0], Integer.parseInt(args[1]));
+        DataOutputStream outServer = new DataOutputStream(sck.getOutputStream());
+
+        BufferedReader inServer = new BufferedReader(new InputStreamReader(sck.getInputStream()));
+
+        msg = inUser.readLine();
+        outServer.writeBytes(msg + "\n");
+
+        msgModified = inServer.readLine();
+        System.out.println("SERVER: " + msgModified);
+
+        sck.close();
     }
 }
--- src/Server.java
+++ src/Server.java
@@ -1,8 +1,27 @@
 /**
  * Created by AndersWOlsen on 31-01-2017.
  */
-public class Server {
-    public static void main(String[] args) {
 
+import java.io.*;
+import java.net.*;
+
+public class Server {
+    public static void main(String[] args) throws Exception {
+        String msg;
+        String msgModified;
+        ServerSocket server = new ServerSocket(Integer.parseInt(args[0]));
+
+        while(true) {
+            Socket connection = server.accept();
+
+            BufferedReader inClient = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            DataOutputStream outClient = new DataOutputStream(connection.getOutputStream());
+
+            msg = inClient.readLine();
+            System.out.println("Received " + msg);
+
+            msgModified = msg.toUpperCase() + "\n";
+            outClient.writeBytes(msgModified);
+        }
     }
 }
